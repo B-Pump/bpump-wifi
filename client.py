4593aa6ce0729cdf71c1d1366fdd6e06352e6dcd
wiizzl <wiizz76.contact@gmail.com>
2024-02-08 13:51:31 +0100

    fix: old py syntax

--- client.py
+++ client.py
@@ -1,18 +1,13 @@
 import socket
 
-# Définir l'adresse IP et le port du serveur auquel se connecter
-SERVER_IP = '192.168.1.100'  # Remplacez ceci par l'adresse IP du serveur
-SERVER_PORT = 12345
+SERVER_IP = "192.168.227.135"
+SERVER_PORT = 5002
 
-# Créer un socket TCP/IP
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Établir une connexion avec le serveur
 client_socket.connect((SERVER_IP, SERVER_PORT))
 
-# Envoyer des données au serveur
-data = "Hello, server!"
+data = "Coucou !!!"
 client_socket.sendall(data.encode())
 
-# Fermer la connexion avec le serveur
 client_socket.close()
